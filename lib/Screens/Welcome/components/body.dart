70173aba450c69d6e6997191cc605a6e28272bb7
Endrju00 <andrzej.kapczynski21@gmail.com>
2022-05-21 23:53:44 +0200

    Create start page

---
+++ lib/Screens/Welcome/components/body.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:plantapp/Screens/Welcome/components/background.dart';
+import 'package:plantapp/constants.dart';
+
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size; // Size of screen
+    return Background(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(height: size.height * 0.2),
+          const Text(
+            "WELCOME TO PLANT APP",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          Image.asset(
+            "assets/images/main_center.png",
+          ),
+          SizedBox(height: size.height * 0.05),
+          SizedBox(
+            height: size.height * 0.05,
+            width: size.width * cMainButtonWidthFactor,
+            child: TextButton(
+              style: ButtonStyle(
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0),
+                  ),
+                ),
+                backgroundColor: MaterialStateProperty.all(cPrimaryColor),
+              ),
+              onPressed: () {},
+              child: const Text("LOGIN", style: TextStyle(color: Colors.white)),
+            ),
+          ),
+          SizedBox(height: size.height * 0.01),
+          SizedBox(
+            height: size.height * 0.05,
+            width: size.width * cMainButtonWidthFactor,
+            child: TextButton(
+              style: ButtonStyle(
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0),
+                  ),
+                ),
+                backgroundColor: MaterialStateProperty.all(cPrimaryLightColor),
+              ),
+              onPressed: () {},
+              child:
+                  const Text("REGISTER", style: TextStyle(color: Colors.white)),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const cPrimaryColor = Color.fromARGB(255, 34, 86, 44);
+const cPrimaryLightColor = Color.fromARGB(255, 114, 162, 118);
+const cMainButtonWidthFactor = 0.65;
