e0e6ff0dbe2799569a99523e059d94075703fdcd
Endrju00 <andrzej.kapczynski21@gmail.com>
2022-05-22 17:42:27 +0200

    Update login screen

--- lib/Screens/Login/components/body.dart
+++ lib/Screens/Login/components/body.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:plantapp/Screens/Login/components/background.dart';
+import 'package:plantapp/components/account_check.dart';
+import 'package:plantapp/components/rounded_button.dart';
 import 'package:plantapp/components/rounded_input_field.dart';
+import 'package:plantapp/components/rounded_password_field.dart';
+import 'package:plantapp/constants.dart';
 
 class Body extends StatelessWidget {
   const Body({
@@ -11,27 +15,36 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Background(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.only(top: size.height * 0.2),
-            child: const Text(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            const Text(
               "LOGIN",
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
-          ),
-          Container(
-            margin: EdgeInsets.only(bottom: size.height * 0.01),
-            child: Image.asset(
-              "assets/images/login_center.png",
+            Container(
+              margin: EdgeInsets.only(bottom: size.height * 0.01),
+              child: Image.asset(
+                "assets/images/login_center.png",
+              ),
             ),
-          ),
-          RoundedInputField(
-            hintText: "Your username",
-            onChanged: (value) {},
-          )
-        ],
+            RoundedInputField(
+              hintText: "Your username",
+              onChanged: (value) {},
+            ),
+            RoundedPasswordField(
+              onChanged: (value) {},
+            ),
+            RoundedButton(
+              text: "LOGIN",
+              press: () {},
+            ),
+            AccountCheck(
+              press: () {},
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/constants.dart
+++ lib/constants.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 const cPrimaryColor = Color.fromARGB(255, 34, 86, 44);
 const cPrimaryLightColor = Color.fromARGB(255, 114, 162, 118);
-const cMainButtonWidthFactor = 0.65;
+const cMainButtonWidthFactor = 0.8;
 const cMainButtonHeightFactor = 0.065;
